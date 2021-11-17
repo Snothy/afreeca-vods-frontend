6b08c7684549256b8f67fa6a42ffda21142527d7
drumevp <drumevp@coventry.ac.uk>
2021-11-18 00:30:00 +0200

    Update config file
    Remove hardcoded heroku url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const dev = false;
 export default {
-  ...(!dev) && {url: 'https://afreeca-backend.herokuapp.com/api/'},
+  ...(!dev) && {url: process.env.backend_url},
   ...(dev) && {url: 'http://localhost:3001/api/'}
 }
