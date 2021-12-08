04a8a55b681cc7b118dbd55c270889dc217c09d8
drumevp <drumevp@coventry.ac.uk>
2021-12-08 16:08:06 +0200

    Update component names to upper case

---
+++ src/components/Vod.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+import { status, json } from '../utilities/requestHandlers';
+import { errorHandler } from '../utilities/errorHandler';
+import ReactPlayer from 'react-player';
+import VodButton from './ListsAndButtons/vodButton';
+import info from '../config';
+import PropTypes from 'prop-types';
+
+class Vod extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      vod: [],
+      url: '',
+      playing: true,
+      mounted: false
+    };
+  }
+
+  static propTypes = {
+    match: PropTypes.object
+  }
+
+  componentDidMount () {
+    this.setState({ mounted: true });
+    const id = this.props.match.params.id;
+    const vodId = this.props.match.params.vodId;
+    const url = info.url + `streamers/${id}/${vodId}`;
+    fetch(url, {
+      method: 'GET',
+      headers: {
+      }
+    })
+      .then(status)
+      .then(json)
+      .then(data => {
+        if (!this.state.mounted) return;
+        if (data.length === undefined) {
+          data = [data];
+        }
+        this.setState({ vod: data });
+        this.setState({ url: data[0].vod_link });
+      })
+      .catch(err => {
+        const error = errorHandler(err);
+        console.log(error);
+        // error handling
+      });
+  }
+
+    changeSrc = (url) => {
+      if (!this.state.mounted) return;
+      this.setState({ url: url });
+    }
+
+    componentWillUnmount () {
+      this.setState({ mounted: false });
+    }
+
+    render () {
+      if (!this.state.vod.length) {
+        return (
+                <>
+                <h2>Loading vod..</h2>
+                </>
+        );
+      }
+
+      const vodLinks = [];
+      for (let i = 0; i < this.state.vod.length; i++) {
+        vodLinks.push(this.state.vod[i].vod_link);
+      }
+      return (
+
+            <>
+            <div style={{
+              paddingLeft: 10,
+              paddingTop: 20,
+              height: 1000
+            }}>
+
+            <ReactPlayer
+            url={this.state.url}
+            playing={this.state.playing}
+            controls={true}
+            style={{
+              float: 'left'
+            }}
+            />
+
+            <VodButton vod={this.state.vod} onStatusChange={this.changeSrc}/>
+            </div>
+
+            </>
+      );
+    }
+}
+
+export default withRouter(Vod);
