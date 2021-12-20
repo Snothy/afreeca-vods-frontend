8973dbe26a75f525c94a1c9e30731efa8e8030ef
Petar Drumev <peter.drumev98@gmail.com>
2021-12-20 12:53:02 +0000

    Update config file for Vercel deployment
    Add gitpod config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const dev = true;
+const dev = false;
 const config = {
   ...(!dev) && { url: process.env.REACT_APP_backend_url },
-  ...(dev) && { url: 'http://localhost:3001/api/' }
+  ...(dev) && { url: 'https://3001-moccasin-python-crsdks3p.ws-eu23.gitpod.io/api/' }
 };
 export default config;
